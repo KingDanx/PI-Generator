c1df1ba864e678ca8ece9582e6b9f4c1825f5ad7
Dan Prudhomme <dannyprud@yahoo.com>
2023-03-29 14:40:41 -0500

    Create vite app, wrap app in react-query, successful request and response to backend

---
+++ PI-Generator/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import axios from "axios";
+import { useQuery } from "react-query";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [auth, setAuth] = useState(null);
+
+  const url = "http://localhost:5000";
+
+  const getToken = async () => {
+    await axios
+      .get(`${url}/auth`)
+      .then((data) => setAuth(data.data.token))
+      .catch((e) => console.log(e));
+  };
+
+  const token = useQuery("auth", getToken);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
