ab6d3d3586ea38512d8ea0df8d7e08852e147451
Dan Prudhomme <dannyprud@yahoo.com>
2023-03-31 16:45:50 -0500

    Move to autocomplete component, stuck trying to get it to work, waiting for more api requests

--- PI-Generator/src/App.jsx
+++ PI-Generator/src/App.jsx
@@ -1,43 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 import SelectParent from "./Components/SelectParent";
 import "./App.css";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  let auth;
   const [realms, setRealms] = useState([]);
   const [region, setRegion] = useState("us");
-  const [realm, setRealm] = useState("us");
+  const [key, setKey] = useState(null);
+  const [realm, setRealm] = useState("Mal'Ganis");
+  const [firstFetch, setFirstFetch] = useState(true);
 
   const url = "http://localhost:5000/auth";
 
   const getToken = async () => {
     await axios
       .get(url)
-      .then((data) => (auth = data.data.token))
+      .then((data) => {
+        if (firstFetch) {
+          setKey(data.data.token);
+          getRealms(data.data.token);
+          setFirstFetch(false);
+        }
+      })
       .catch((e) => console.log(e));
   };
 
-  const getRealms = async () => {
+  const getRealms = async (token) => {
     await axios
       .get(
-        `https://${region}.api.blizzard.com/data/wow/search/connected-realm?namespace=dynamic-${region}&orderby=name&_page=1&access_token=${auth}`
+        `https://${region}.api.blizzard.com/data/wow/search/realm?namespace=dynamic-${region}&orderby=realmname:asc&_pageSize=500&_page=1&access_token=${token}`
       )
       .then((data) => {
         console.log(data.data.results);
-        setRealms(data.data.results);
-      });
+        setRealms(
+          data.data.results.map((el) => (el = el.data.name.en_US)).sort()
+        );
+      })
+      .catch((e) => console.log(e));
   };
 
   const token = useQuery("auth", getToken);
 
-  const realmList = useQuery("realms", getRealms);
+  // const realmList = useQuery("realms", getRealms());
+
+  useEffect(() => {
+    token.refetch();
+  }, []);
 
   return (
     <div>
-      <SelectParent auth={auth} realms={realms} realm={realm} region={region} />
+      <SelectParent
+        realms={realms}
+        realm={realm}
+        region={region}
+        setRegion={setRegion}
+        setRealm={setRealm}
+        setRealms={setRealms}
+        getToken={getToken}
+        tokener={key}
+      />
     </div>
   );
 };
--- PI-Generator/src/Components/SelectParent.jsx
+++ PI-Generator/src/Components/SelectParent.jsx
@@ -4,26 +4,57 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 import axios from "axios";
 
-const SelectParent = ({ realms, realm, region }) => {
-  const handleChange = (event, setState) => {
+const SelectParent = ({
+  realms,
+  realm,
+  region,
+  setRegion,
+  setRealms,
+  setRealm,
+  tokener,
+}) => {
+  const [realmText, setRealmText] = useState("doggoshit");
+  const realmSelect = useRef();
+  // const realmText = useRef();
+  // console.log(refetchRef.current);
+  // refetch();
+  const getRealms = async (region, token) => {
+    await axios
+      .get(
+        `https://${region}.api.blizzard.com/data/wow/search/realm?namespace=dynamic-${region}&orderby=realmname:asc&_pageSize=500&_page=1&access_token=${token}`
+      )
+      .then((data) => {
+        console.log(data.data.results);
+        setRealms(
+          data.data.results.map((el) => (el = el.data.name.en_US)).sort()
+        );
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const handleChange = (event, setState, callback) => {
     setState(event.target.value);
   };
 
-  console.log(realms);
+  // console.log(realms);
   return (
-    <div>
+    <div style={{ display: "flex" }}>
       {/* Region Select */}
       <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
+        <FormControl>
           <InputLabel id="region">Region</InputLabel>
           <Select
             labelId="region"
             id="region"
             value={region}
             label="region"
-            onChange={(e) => handleChange(e, setRegion)}
+            onChange={(e) =>
+              handleChange(e, setRegion, getRealms(e.target.value, tokener))
+            }
           >
             <MenuItem value={"us"}>US</MenuItem>
             <MenuItem value={"eu"}>EU</MenuItem>
@@ -32,24 +63,44 @@ const SelectParent = ({ realms, realm, region }) => {
       </Box>
       {/* Realm Select */}
       <Box sx={{ minWidth: 120 }}>
-        <FormControl fullWidth>
-          <InputLabel id="realm">Realm</InputLabel>
-          <Select
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={realms}
+          // value={realm}
+          sx={{ width: 300 }}
+          renderInput={(params) => <TextField {...params} label="Realms" />}
+          onChange={(e) => handleChange(e, setRealm)}
+        />
+        {/* <Select
             labelId="realm"
             id="realm"
             value={realm}
             label="realm"
             onChange={(e) => handleChange(e, setRealm)}
+            inputRef={realmSelect}
+            disableTyping
           >
+            <TextField
+              id="outlined-basic"
+              label="Outlined"
+              variant="outlined"
+              // inputRef={realmText}
+              onInput={(e) => handleChange(e, setRealmText)}
+            />
             {!realms
               ? null
-              : realms.map((el, i) => (
-                  <MenuItem value={el.data.realms[0].name.en_US || "unknown"}>
-                    {`${el.data?.realms[0]?.name?.en_US}`}
-                  </MenuItem>
-                ))}
-          </Select>
-        </FormControl>
+              : realms.map((el, i) =>
+                  el.data.name.en_US.includes(realmText) ? (
+                    <MenuItem
+                      key={el.data.name.en_US}
+                      value={el.data.name.en_US}
+                    >
+                      {`${el.data?.name?.en_US}`}
+                    </MenuItem>
+                  ) : null
+                )}
+          </Select> */}
       </Box>
     </div>
   );
